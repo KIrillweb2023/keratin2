6eb24782e403663d2d45fac07316a2506dae77ad
Gleb <glebneshik@gmail.com>
2025-02-19 01:37:23 +0500

    Slider-swipper

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,41 @@
-import Swiper from "swiper"
+const swiper = new Swiper('.reviews__swiper', {
+    loop: true,
+    slidesPerView: 'auto', 
+    spaceBetween: 30,  
+    speed: 700,
 
-// window.addEventListener("DOMContentLoaded", () => {
-    console.log("ok")
-  
-    const swiper = new Swiper(".swiper", {
-        direction: 'horizontal',
-        loop: false,
-        slidesPerView: 1,
-        spaceBetween: 10
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 10,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
 
-        // pagination: {
-        //     el: '.swiper-pagination',
-        // },
 
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
+    pagination: {
+        el: '.reviews__pagination',
+        clickable: true,      
+    },
 
-        // scrollbar: {
-        //     el: '.swiper-scrollbar',
-        // },
-    })
-// })
+    navigation: {
+        nextEl: '.reviews__swiper-button-next',
+        prevEl: '.reviews__swiper-button-prev',
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
