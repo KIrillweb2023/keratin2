9fe10de3ae75a70f5ff3d90132ff5e907f726abe
KIrillweb2023 <webrazrabotka72@gmail.com>
2025-02-18 19:54:29 +0500

    19.54 18.02.2024

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,25 @@
-"use strict"
+import Swiper from "swiper"
 
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("This OKEY!")
-})
+// window.addEventListener("DOMContentLoaded", () => {
+    console.log("ok")
+  
+    const swiper = new Swiper(".swiper", {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 1,
+        spaceBetween: 10
+
+        // pagination: {
+        //     el: '.swiper-pagination',
+        // },
+
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    })
+// })
