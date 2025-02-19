f883d54a803f2896c87227285528e7683471a586
KIrillweb2023 <webrazrabotka72@gmail.com>
2025-02-19 17:04:42 +0500

    Slider-tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,61 @@
-const swiper = new Swiper('.reviews__swiper', {
-    loop: true,
-    slidesPerView: 'auto', 
-    spaceBetween: 30,  
-    speed: 700,
-
-    effect: 'coverflow',
-    coverflowEffect: {
-        rotate: 10,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
+const slider = document.querySelector(".reviews-slider");
+const sliderContainer = document.querySelector(".reviews-slider-container");
+const containerSlide = document.querySelectorAll(".reviews__slide");
+const prevbutton = document.querySelector(".reviews__navigation-button-prev")
+const nextbutton = document.querySelector(".reviews__navigation-button-next")
+const pagination = document.querySelector(".reviews__pagination");
+const tabsPagination = document.querySelectorAll(".reviews__pagination-tab");
 
 
-    pagination: {
-        el: '.reviews__pagination',
-        clickable: true,      
-    },
+// function foreachChilds() {
+    containerSlide.forEach((item, index) => {
+        item.style.width = 309 + "px"
+    })
 
-    navigation: {
-        nextEl: '.reviews__swiper-button-next',
-        prevEl: '.reviews__swiper-button-prev',
-    },
+function slideTab(index) {
+    tabsPagination.forEach((item, index) => {
+        item.classList.remove("reviews__pagination-tab-active")
+    })
+    tabsPagination[index - 1].classList.add("reviews__pagination-tab-active")
+    // console.log(tabsPagination[0].classList.add("reviews__pagination-tab-active"))
+}
+  
 
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 40
-        }
+// }
+
+let indexSlide = 1;
+slideTab(indexSlide)
+nextbutton.addEventListener('click', (e) => {
+   
+    if(indexSlide < 3) {
+        indexSlide++
+        sliderContainer.style.transform += `translate(-${1360}px)`
+        slideTab(indexSlide)
+    } else {
+        indexSlide = 1;
+        sliderContainer.style.transform = `translate(0px)`
+        slideTab(indexSlide)
     }
-});
+})
+prevbutton.addEventListener('click', (e) => {
+   
+    if(indexSlide === 1) {
+        indexSlide = 3
+        sliderContainer.style.transform += `translate(-${1360 * 2}px)`
+        slideTab(indexSlide)
+    } else {
+        indexSlide--
+        sliderContainer.style.transform += `translate(${1360}px)`
+        slideTab(indexSlide)
+    }
+})
+function resizeWidthContainer() {
+    console.log(slider.clientWidth)
+    sliderContainer.style.width = `${slider.clientWidth + 2712}px`
+}
+// foreachChilds()
+resizeWidthContainer()
+window.addEventListener("resize", (e) => {
+    console.log("ok")
+    resizeWidthContainer()
+})
