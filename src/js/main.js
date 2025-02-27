906d89b387b4ab56fa770421c30d45237c87ef46
KIrillweb2023 <webrazrabotka72@gmail.com>
2025-02-27 19:34:10 +0500

    19.33 27.02.2025

--- src/js/main.js
+++ src/js/main.js
@@ -1,132 +1,3 @@
-// const slider = document.querySelector(".reviews-slider");
-// const sliderContainer = document.querySelector(".reviews-slider-container");
-// const containerSlide = document.querySelectorAll(".reviews__slide");
-// const prevbutton = document.querySelector(".reviews__navigation-button-prev")
-// const nextbutton = document.querySelector(".reviews__navigation-button-next")
-// const pagination = document.querySelector(".reviews__pagination");
-// const tabsPagination = document.querySelectorAll(".reviews__pagination-tab");
-// const containerSite = document.querySelector(".container");
-
-
-// let indexSlide = 0;
-// let slideScrollWidth;
-
-// if(window.innerWidth <= 1500) {
-//     slideScrollWidth = (sliderContainer.clientWidth / 12) * 3;
-// } else if(window.innerWidth <= 1300) {
-//     slideScrollWidth = (sliderContainer.clientWidth / 12) * 2;
-// } else if(window.innerWidth <= 850) {
-//     slideScrollWidth = (sliderContainer.clientWidth / 12);
-// } else {
-//     slideScrollWidth = (sliderContainer.clientWidth / 12) * 4
-// }
-
-
-// window.addEventListener("resize", (e) => {
-//     console.log(slideScrollWidth)
-//     // resizeWidthContainer()
-  
-//     slideScrollWidth = (sliderContainer.clientWidth / 12) * 4
-
-//     if(window.innerWidth <= 1500) {
-//         slideScrollWidth = (sliderContainer.clientWidth / 12) * 3;
-//     } else if(window.innerWidth <= 1300) {
-//         slideScrollWidth = (sliderContainer.clientWidth / 12) * 2;
-//     } else if(window.innerWidth <= 850) {
-//         slideScrollWidth = (sliderContainer.clientWidth / 4);
-//     }else {
-//         slideScrollWidth = (sliderContainer.clientWidth / 12) * 4
-//     }
-// })
- 
-
-// // containerSlide.forEach((item, index) => {
-// //     item.style.width = `${slideWidth}px`
-// // })
-
-// function slideTab(index) {
-//     tabsPagination.forEach((item, index) => {
-//         item.classList.remove("reviews__pagination-tab-active")
-//     })
-//     tabsPagination[index].classList.add("reviews__pagination-tab-active")
-// }
-
-
-// function slideTabClick() {
-//     tabsPagination.forEach((item, index) => {
-//         // item.classList.remove("reviews__pagination-tab-active")
-
-//         item.addEventListener('click', () => {
-//             indexSlide = index
-//             slideTab(index)
-//             console.log(indexSlide)
-//             if(indexSlide === 1) {
-//                  sliderContainer.style.transform = `translate(-${slideScrollWidth}px)`
-//             } else if (indexSlide === 2) {
-//                 sliderContainer.style.transform = `translate(-${slideScrollWidth * 2}px)`
-//             } else if (indexSlide === 0) {
-//                  sliderContainer.style.transform = `translate(0px)`
-//             }
-//         })
-//     })
-// }
-// slideTabClick()
-
-// // }
-
-// let resultvalueSlider = 0
-// slideTab(indexSlide)
-// nextbutton.addEventListener('click', (e) => {
-//      sliderContainer.style.transform = `translate(0px)`
-//     if(indexSlide < 2) {
-//         indexSlide++
-
-//         sliderContainer.style.transform = `translate(-${resultvalueSlider += slideScrollWidth}px)`
-//         slideTab(indexSlide)
-//     } else {
-//         resultvalueSlider = 0
-//         indexSlide = 0;
-//         sliderContainer.style.transform = `translate(0px)`
-//         slideTab(indexSlide)
-//     }
-// })
-// prevbutton.addEventListener('click', (e) => {
-//     if(indexSlide === 0) {
-//         indexSlide = 2
-//         sliderContainer.style.transform = `translate(-${resultvalueSlider += slideScrollWidth * 2}px)`
-//         slideTab(indexSlide)
-     
-//     } else {
-
-//         indexSlide--
-//         sliderContainer.style.transform = `translate(-${resultvalueSlider -= slideScrollWidth  }px)`
-//         slideTab(indexSlide)
-//     }
-// })
-
-// window.onload = function(){
-//     document.getElementById("video").play()
-// }
-
-import Swiper from "swiper";
-
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 4,  // Показывать 4 слайда
-    slidesPerGroup: 4, // Перелистывать по 4 слайда
-    // spaceBetween: 30,   // Расстояние между слайдами (необязательно)
-    loop: false,       // Не зацикливать слайдер
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
-
-
-
 let item = document.querySelectorAll(".question-item");
 
 item.forEach((item) => {
@@ -141,5 +12,43 @@ item.forEach((item) => {
             item.classList.remove("question-item_active")
             item.childNodes[1].childNodes[3].style.transform = "rotate(0deg)";
        }
+    })
+})
+
+
+const hamburger = document.querySelector(".header-hamburger");
+const elementMenu = document.querySelector(".header-nav")
+const elementMenuTab = document.querySelectorAll(".header-nav-list_item__link")
+
+hamburger.addEventListener("click", (e) => {
+    elementMenu.classList.toggle("header-nav_active")
+    if(elementMenu.classList.contains("header-nav_active")) {
+        document.body.style.overflow = "hidden"
+    } else {
+        document.body.style.overflow = ""
+    }
+})
+
+elementMenuTab.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        elementMenu.classList.remove("header-nav_active");
+          document.body.style.overflow = "";
+    })
+})
+
+
+const videoPlayBtn = document.querySelector(".material-wrapper");
+const video = document.getElementById("video");
+
+videoPlayBtn.addEventListener("click", (e) => {
+    video.play()
+})
+
+
+const footerTemplate = document.querySelectorAll(".footer-template");
+
+footerTemplate.forEach((item) => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("footer-template_active");
     })
 })
